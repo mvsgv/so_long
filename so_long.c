37fbdaf2a46db978c4321bdcf372fcb11d7f31d1
mskn <167631184+mvsgv@users.noreply.github.com>
2024-11-30 15:13:01 +0100

    Update so_long.c

--- so_long.c
+++ so_long.c
@@ -31,3 +31,55 @@ int main(int argc, char **argv)
     mlx_loop(game.mlx);
     return (0);
 }
+int main(void)
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+t_asset asset;
+// Initialisation de la bibliothèque mlx
+asset.mlx = mlx_init();
+if (tasset.mlx)
+return (1);
+// Création de la fenêtre (800x600 ici, vous pouvez ajuster les dimensions) asset-win = mlx_new_window(asset.mlx, 800, 600, "Fenêtre avec image");
+if (lasset.win)
+return (1);
+// Charger une image XPM
+asset. img = mlx_xpm_file_to_image(asset.mlx,
+if (lasset.img)
+"asset/sp-xpm", sasset.img_width,
+Sgsset. img_height) ;
+mlx_ destroy_window(asset.mlx, asset.win);
+return (1);
+// Afficher l'image dans la fenêtre, à la position (0, 0)
+mix_put_image_to_window(asset.mx, asset.win, asset.img, 0, 0);
+// Gérer la fermeture de la fenêtre
+mx hook(asset.win, 17, 0, close_window, &asset);
+// Lancer la boucle principale de la bibliothèque mlx
+mlx_loop (asset.mlx) ;
+53
+return (0);
+AA FUNCTION LINES: 30 44
+
